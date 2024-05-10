9dc0dad8e7ac75f50d06e2f6d42dbdc25a564d6d
kaJseia <89431075+mur4i@users.noreply.github.com>
2024-05-10 20:13:18 -0300

    update para base

--- config.lua
+++ config.lua
@@ -1,7 +1,7 @@
 return {
     FuelScript = {
         enable = false,
-        script = 'LegacyFuel',
+        script = 'cdn-fuel',
     },
     BossModel = `s_m_y_construct_02`,
     BossCoords = vec4(884.47, -2337.14, 29.34, 359.1),
